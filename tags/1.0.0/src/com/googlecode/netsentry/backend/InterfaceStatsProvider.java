355743024314f6b442d49cc72cae9c78bcd2e565
lorenz.fischer <lorenz.fischer@c066fec4-18a9-11de-9c9a-2d325a3e6494>
2009-03-25 00:00:05 +0000

    tag for release 1.0.0

---
+++ tags/1.0.0/src/com/googlecode/netsentry/backend/InterfaceStatsProvider.java
@@ -0,0 +1,379 @@
+/**
+ * 
+ */
+package com.googlecode.netsentry.backend;
+
+import java.util.HashMap;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import com.googlecode.netsentry.R;
+
+/**
+ * <p>
+ * This content provider can be used to retrieve information about the bytes
+ * sent and received by the network interfaces of an android phone.
+ * </p>
+ * 
+ * <p>
+ * Adapted from the com.example.android.notepad.NotePadProvider example.
+ * </p>
+ * 
+ * @author lorenz fischer
+ */
+public class InterfaceStatsProvider extends ContentProvider {
+
+    // /** The tag information for the logging facility. */
+    // private static final String TAG = "ns.InterfaceStatsProvider";
+
+    /** The authority is used so we can put together the content provider uri. */
+    public static final String AUTHORITY = "com.googlecode.netsentry";
+
+    /**
+     * The content:// style URL for this table.
+     */
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/interfacestats");
+
+    /**
+     * The MIME type of {@link #CONTENT_URI} providing a directory of interface
+     * statisics entries.
+     */
+    public static final String CONTENT_TYPE = "vnd.android.cursor.dir/interfacestats";
+
+    /**
+     * The MIME type of a {@link #CONTENT_URI} sub-directory of a single
+     * interface statistics entry.
+     */
+    public static final String ITEM_CONTENT_TYPE = "vnd.android.cursor.item/interfacestats";
+
+    // these values are used to map interface names to interface types
+    /** The matching group for wifi type interface names. */
+    public final static String INTERFACE_NAME_TYPE_WIFI = "tiwlan";
+
+    /** The matching group for 3g type interface names. */
+    public final static String INTERFACE_NAME_TYPE_3G = "rmnet";
+
+    /** The matching group for ethernet type interface names. */
+    public final static String INTERFACE_NAME_TYPE_ETHERNET = "eth";
+
+    /** The name of the database file. */
+    private static final String DATABASE_NAME = "netsentry.db";
+
+    /**
+     * The version of the database. 1. Initial Version
+     */
+    private static final int DATABASE_VERSION = 1;
+
+    /** The name of the table holding the interface statistics records. */
+    private static final String INTERFACE_STATS_TABLE_NAME = "InterfaceStats";
+
+    /**
+     * The uri matcher helps to parse uri strings sent by clients. TODO is
+     * UriMatcher thread safe? Need to know for {@link #getType(Uri)} needs to
+     * be thread safe..
+     */
+    private static final UriMatcher uriMatcher;
+
+    private static final int INTERFACESTATS = 1;
+    private static final int INTERFACESTATS_ID = 2;
+
+    /** The projection map. */
+    private static final HashMap<String, String> sProjectionMap;
+
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(InterfaceStatsProvider.AUTHORITY, "interfacestats", INTERFACESTATS);
+        uriMatcher.addURI(InterfaceStatsProvider.AUTHORITY, "interfacestats/#", INTERFACESTATS_ID);
+
+        sProjectionMap = new HashMap<String, String>();
+        sProjectionMap.put(InterfaceStatsColumns._ID, InterfaceStatsColumns._ID);
+        sProjectionMap.put(InterfaceStatsColumns.INTERFACE_NAME,
+                InterfaceStatsColumns.INTERFACE_NAME);
+        sProjectionMap.put(InterfaceStatsColumns.INTERFACE_ALIAS,
+                InterfaceStatsColumns.INTERFACE_ALIAS);
+        sProjectionMap.put(InterfaceStatsColumns.BYTES_RECEIVED,
+                InterfaceStatsColumns.BYTES_RECEIVED);
+        sProjectionMap.put(InterfaceStatsColumns.BYTES_RECEIVED_SYSTEM,
+                InterfaceStatsColumns.BYTES_RECEIVED_SYSTEM);
+        sProjectionMap.put(InterfaceStatsColumns.BYTES_SENT, InterfaceStatsColumns.BYTES_SENT);
+        sProjectionMap.put(InterfaceStatsColumns.BYTES_SENT_SYSTEM,
+                InterfaceStatsColumns.BYTES_SENT_SYSTEM);
+        sProjectionMap.put(InterfaceStatsColumns.BYTES_TRANSMISSION_LIMIT,
+                InterfaceStatsColumns.BYTES_TRANSMISSION_LIMIT);
+        sProjectionMap.put(InterfaceStatsColumns.RESET_CRON_EXPRESSION,
+                InterfaceStatsColumns.RESET_CRON_EXPRESSION);
+        sProjectionMap.put(InterfaceStatsColumns.SHOW_IN_LIST, InterfaceStatsColumns.SHOW_IN_LIST);
+        sProjectionMap.put(InterfaceStatsColumns.NOTIFICATION_LEVEL,
+                InterfaceStatsColumns.NOTIFICATION_LEVEL);
+        sProjectionMap.put(InterfaceStatsColumns.LAST_UPDATE, InterfaceStatsColumns.LAST_UPDATE);
+        sProjectionMap.put(InterfaceStatsColumns.LAST_RESET, InterfaceStatsColumns.LAST_RESET);
+    }
+
+    /** Helper for the creation and updates of the database table. */
+    private DatabaseHelper databaseHelper;
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean onCreate() {
+        this.databaseHelper = new DatabaseHelper(getContext());
+        return true; // successfully loaded
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+            String sortOrder) {
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+        switch (InterfaceStatsProvider.uriMatcher.match(uri)) {
+        case INTERFACESTATS:
+            qb.setTables(INTERFACE_STATS_TABLE_NAME);
+            qb.setProjectionMap(InterfaceStatsProvider.sProjectionMap);
+            break;
+
+        case INTERFACESTATS_ID:
+            qb.setTables(INTERFACE_STATS_TABLE_NAME);
+            qb.setProjectionMap(InterfaceStatsProvider.sProjectionMap);
+            qb.appendWhere(InterfaceStatsColumns._ID + "=" + uri.getPathSegments().get(1));
+            break;
+
+        default:
+            throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        // If no sort order is specified use the default
+        String orderBy;
+        if (TextUtils.isEmpty(sortOrder)) {
+            orderBy = InterfaceStatsColumns.DEFAULT_SORT_ORDER;
+        } else {
+            orderBy = sortOrder;
+        }
+
+        // Get the database and run the query
+        SQLiteDatabase db = this.databaseHelper.getReadableDatabase();
+        Cursor c = qb.query(db, projection, selection, selectionArgs, null, null, orderBy);
+
+        // Tell the cursor what uri to watch, so it knows when its source data
+        // changes
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+        return c;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Uri insert(Uri uri, ContentValues initialValues) {
+        ContentValues values;
+        SQLiteDatabase db;
+        long rowId;
+
+        // Validate the requested uri
+        if (InterfaceStatsProvider.uriMatcher.match(uri) != INTERFACESTATS) {
+            throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        values = new ContentValues(initialValues);
+
+        // fill in default values (if not set already)
+        setDefaultInterfaceStatsValues(values);
+
+        // Make sure that the fields are all set
+        if (!values.containsKey(InterfaceStatsColumns.INTERFACE_NAME)) {
+            throw new IllegalArgumentException("Could not store entry: missing interface name.");
+        }
+
+        if (!values.containsKey(InterfaceStatsColumns.INTERFACE_ALIAS)) {
+            // use interface name as default interface alias
+            values.put(InterfaceStatsColumns.INTERFACE_ALIAS, values
+                    .getAsString(InterfaceStatsColumns.INTERFACE_NAME));
+        }
+
+        db = this.databaseHelper.getWritableDatabase();
+        rowId = db.insert(InterfaceStatsProvider.INTERFACE_STATS_TABLE_NAME,
+                InterfaceStatsColumns.RESET_CRON_EXPRESSION, values);
+        if (rowId > 0) {
+            Uri interfaceStatsUri = ContentUris.withAppendedId(InterfaceStatsProvider.CONTENT_URI,
+                    rowId);
+            getContext().getContentResolver().notifyChange(interfaceStatsUri, null);
+            return interfaceStatsUri;
+        }
+
+        throw new SQLException("Failed to insert row into " + uri);
+    }
+
+    /**
+     * This method fills in all default values needed for a single record of
+     * type {@link #INTERFACESTATS}.
+     */
+    private static void setDefaultInterfaceStatsValues(ContentValues values) {
+        if (!values.containsKey(InterfaceStatsColumns.BYTES_RECEIVED)) {
+            values.put(InterfaceStatsColumns.BYTES_RECEIVED, Long.valueOf(0));
+        }
+        if (!values.containsKey(InterfaceStatsColumns.BYTES_RECEIVED_SYSTEM)) {
+            values.put(InterfaceStatsColumns.BYTES_RECEIVED_SYSTEM, Long.valueOf(0));
+        }
+        if (!values.containsKey(InterfaceStatsColumns.BYTES_SENT)) {
+            values.put(InterfaceStatsColumns.BYTES_SENT, Long.valueOf(0));
+        }
+        if (!values.containsKey(InterfaceStatsColumns.BYTES_SENT_SYSTEM)) {
+            values.put(InterfaceStatsColumns.BYTES_SENT_SYSTEM, Long.valueOf(0));
+        }
+        if (!values.containsKey(InterfaceStatsColumns.BYTES_TRANSMISSION_LIMIT)) {
+            // the default transmission limit is 0 = unlimited
+            values.put(InterfaceStatsColumns.BYTES_TRANSMISSION_LIMIT, Long.valueOf(0));
+        }
+        if (!values.containsKey(InterfaceStatsColumns.NOTIFICATION_LEVEL)) {
+            values.put(InterfaceStatsColumns.NOTIFICATION_LEVEL, Long.valueOf(0));
+        }
+        if (!values.containsKey(InterfaceStatsColumns.SHOW_IN_LIST)) {
+            // not hidden is default
+            values.put(InterfaceStatsColumns.SHOW_IN_LIST, Long.valueOf(1));
+        }
+        if (!values.containsKey(InterfaceStatsColumns.LAST_UPDATE)) {
+            values.put(InterfaceStatsColumns.LAST_UPDATE, Long.valueOf(System.currentTimeMillis()));
+        }
+        if (!values.containsKey(InterfaceStatsColumns.LAST_RESET)) {
+            values.put(InterfaceStatsColumns.LAST_RESET, Long.valueOf(System.currentTimeMillis()));
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int delete(Uri uri, String where, String[] whereArgs) {
+        SQLiteDatabase db = this.databaseHelper.getWritableDatabase();
+        int count;
+        switch (InterfaceStatsProvider.uriMatcher.match(uri)) {
+        case InterfaceStatsProvider.INTERFACESTATS:
+            count = db.delete(INTERFACE_STATS_TABLE_NAME, where, whereArgs);
+            break;
+
+        case InterfaceStatsProvider.INTERFACESTATS_ID:
+            String noteId = uri.getPathSegments().get(1);
+            count = db.delete(InterfaceStatsProvider.INTERFACE_STATS_TABLE_NAME,
+                    InterfaceStatsColumns._ID + "=" + noteId
+                            + (!TextUtils.isEmpty(where) ? " AND (" + where + ')' : ""), whereArgs);
+            break;
+
+        default:
+            throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int update(Uri uri, ContentValues values, String where, String[] whereArgs) {
+        SQLiteDatabase db = this.databaseHelper.getWritableDatabase();
+        int count;
+        switch (InterfaceStatsProvider.uriMatcher.match(uri)) {
+        case InterfaceStatsProvider.INTERFACESTATS:
+            count = db.update(InterfaceStatsProvider.INTERFACE_STATS_TABLE_NAME, values, where,
+                    whereArgs);
+            break;
+
+        case InterfaceStatsProvider.INTERFACESTATS_ID:
+            String noteId = uri.getPathSegments().get(1);
+            count = db.update(InterfaceStatsProvider.INTERFACE_STATS_TABLE_NAME, values,
+                    InterfaceStatsColumns._ID + "=" + noteId
+                            + (!TextUtils.isEmpty(where) ? " AND (" + where + ')' : ""), whereArgs);
+            break;
+
+        default:
+            throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getType(Uri uri) {
+        String result;
+
+        switch (uriMatcher.match(uri)) {
+        case INTERFACESTATS:
+            result = InterfaceStatsProvider.CONTENT_TYPE;
+            break;
+
+        case INTERFACESTATS_ID:
+            result = InterfaceStatsProvider.ITEM_CONTENT_TYPE;
+            break;
+
+        default:
+            throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        return result;
+    }
+
+    /**
+     * This class helps open, create, and upgrade the database file.
+     */
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+
+        private final Context mContext;
+
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+            mContext = context;
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            ContentValues valuesLoopback = new ContentValues();
+
+            // Log.v(TAG, "Creating database for interface statistics.");
+
+            // TODO what happens when the database creation failes
+            db.execSQL("CREATE TABLE " + INTERFACE_STATS_TABLE_NAME + " ("
+                    + InterfaceStatsColumns._ID + " INTEGER PRIMARY KEY,"
+                    + InterfaceStatsColumns.INTERFACE_NAME + " TEXT,"
+                    + InterfaceStatsColumns.INTERFACE_ALIAS + " TEXT,"
+                    + InterfaceStatsColumns.BYTES_RECEIVED + " INTEGER,"
+                    + InterfaceStatsColumns.BYTES_RECEIVED_SYSTEM + " INTEGER,"
+                    + InterfaceStatsColumns.BYTES_SENT + " INTEGER,"
+                    + InterfaceStatsColumns.BYTES_SENT_SYSTEM + " INTEGER,"
+                    + InterfaceStatsColumns.BYTES_TRANSMISSION_LIMIT + " INTEGER,"
+                    + InterfaceStatsColumns.RESET_CRON_EXPRESSION + " TEXT,"
+                    + InterfaceStatsColumns.SHOW_IN_LIST + " INTEGER,"
+                    + InterfaceStatsColumns.NOTIFICATION_LEVEL + " INTEGER,"
+                    + InterfaceStatsColumns.LAST_UPDATE + " INTEGER,"
+                    + InterfaceStatsColumns.LAST_RESET + " INTEGER" + ");");
+
+            // create the loop back device which will be hidden by default
+            valuesLoopback.put(InterfaceStatsColumns.INTERFACE_NAME, "lo");
+            valuesLoopback.put(InterfaceStatsColumns.INTERFACE_ALIAS, mContext
+                    .getString(R.string.provider_interface_alias_loopback));
+            valuesLoopback.put(InterfaceStatsColumns.SHOW_IN_LIST, Long.valueOf(0));
+            valuesLoopback.put(InterfaceStatsColumns.BYTES_TRANSMISSION_LIMIT, Long.valueOf(0));
+            InterfaceStatsProvider.setDefaultInterfaceStatsValues(valuesLoopback);
+            db.insert(InterfaceStatsProvider.INTERFACE_STATS_TABLE_NAME,
+                    InterfaceStatsColumns.RESET_CRON_EXPRESSION, valuesLoopback);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // Log.w(TAG, "Upgrading database from version " + oldVersion +
+            // " to " + newVersion
+            // + ", which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS " + InterfaceStatsProvider.INTERFACE_STATS_TABLE_NAME);
+            onCreate(db);
+        }
+    }
+
+}
---
+++ tags/1.0.0/src/com/googlecode/netsentry/ui/InterfaceStatsList.java
@@ -0,0 +1,320 @@
+package com.googlecode.netsentry.ui;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.app.Dialog;
+import android.app.ListActivity;
+import android.app.NotificationManager;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ResourceCursorAdapter;
+import android.widget.TextView;
+
+import com.googlecode.netsentry.R;
+import com.googlecode.netsentry.backend.Bootstrapper;
+import com.googlecode.netsentry.backend.Configuration;
+import com.googlecode.netsentry.backend.InterfaceStatsColumns;
+import com.googlecode.netsentry.backend.InterfaceStatsProvider;
+import com.googlecode.netsentry.backend.Resetter;
+import com.googlecode.netsentry.backend.Updater;
+import com.googlecode.netsentry.util.StringUtilities;
+
+/**
+ * This activity lists all the records of the {@link InterfaceStatsProvider}.
+ * 
+ * @author lorenz fischer
+ */
+public class InterfaceStatsList extends ListActivity {
+
+    // /** TAG for logging. */
+    // private static final String TAG = "ns.InterfaceList";
+
+    /**
+     * Standard projection for the interesting columns of a normal interface
+     * stats record.
+     */
+    private static final String[] PROJECTION = new String[] { InterfaceStatsColumns._ID, // 0
+            InterfaceStatsColumns.INTERFACE_NAME, // 1
+            InterfaceStatsColumns.INTERFACE_ALIAS, // 2
+            InterfaceStatsColumns.BYTES_RECEIVED, // 3
+            InterfaceStatsColumns.BYTES_SENT, // 4
+            InterfaceStatsColumns.BYTES_TRANSMISSION_LIMIT, // 5
+            InterfaceStatsColumns.LAST_UPDATE, // 6
+            InterfaceStatsColumns.LAST_RESET // 7
+    };
+
+    /** The id for the "reset counters" context menu item. */
+    private static final int CONTEXT_MENU_ITEM_RESET_COUNTERS = Menu.FIRST;
+
+    /** The id for the "edit" context menu item. */
+    private static final int CONTEXT_MENU_ITEM_EDIT = Menu.FIRST + 1;
+
+    /** The id for the "about" option menu item. */
+    private static final int OPTION_MENU_ITEM_PREFERENCES = Menu.FIRST;
+
+    /** The id for the "about" option menu item. */
+    private static final int OPTION_MENU_ITEM_ABOUT = Menu.FIRST + 1;
+
+    /** Constant for the set transmission limit dialog. */
+    private static final int DIALOG_ABOUT = 1;
+
+    /**
+     * The cursor through which the database will be accessed, this cursor will
+     * be closed in the {@link #onDestroy()} method.
+     */
+    private Cursor mCursor;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        NotificationManager notificationManager;
+
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // If no data was given in the intent (because we were started
+        // as a MAIN activity), then use our default content provider.
+        Intent intent = getIntent();
+        if (intent.getData() == null) {
+            intent.setData(InterfaceStatsProvider.CONTENT_URI);
+        }
+
+        // Inform the list we provide context menus for items
+        getListView().setOnCreateContextMenuListener(this);
+
+        /*
+         * Perform a managed query. The Activity will handle closing and
+         * requerying the cursor when needed.
+         */
+        mCursor = managedQuery(getIntent().getData(), PROJECTION,
+                InterfaceStatsColumns.SHOW_IN_LIST + " = 1", null,
+                InterfaceStatsColumns.DEFAULT_SORT_ORDER);
+
+        // Used to map the rows in the cursor to list views.
+        ResourceCursorAdapter adapter = new ResourceCursorAdapter(this,
+                R.layout.interfacestats_list_item, mCursor) {
+
+            private DateFormat mFormat = new SimpleDateFormat();
+
+            @Override
+            public void bindView(View view, Context context, Cursor cursor) {
+                final int usageLevelMedium = Configuration
+                        .getUsageThresholdMedium(InterfaceStatsList.this);
+                final int usageLevelHigh = Configuration
+                        .getUsageThresholdHigh(InterfaceStatsList.this);
+                ImageView typeIcon = (ImageView) view
+                        .findViewById(R.id.interfaceList_Item_image_interfaceType);
+                TextView alias = (TextView) view
+                        .findViewById(R.id.interfaceList_Item_interfaceAlias);
+                TextView counter = (TextView) view.findViewById(R.id.interfaceList_Item_counter);
+                TextView lastResetValue = (TextView) view
+                        .findViewById(R.id.interfaceList_Item_lastReset_value);
+                long bytesTotal;
+                long bytesLimit;
+
+                bytesTotal = cursor.getLong(3) + cursor.getLong(4);
+                bytesLimit = cursor.getLong(5);
+
+                typeIcon.setImageResource(InterfaceIcon.getResourceIdForInterface(cursor
+                        .getString(1)));
+                alias.setText(cursor.getString(2));
+
+                lastResetValue.setText(mFormat.format(new Date(cursor.getLong(7))));
+
+                if (bytesLimit > 0) {
+                    long usage;
+
+                    usage = Double.valueOf((1.0D * bytesTotal / bytesLimit) * 100).longValue();
+
+                    // set color according to usage
+                    if (usage >= usageLevelHigh) {
+                        counter.setTextColor(getResources().getColor(R.color.solid_red));
+                    } else if (usage >= usageLevelMedium) {
+                        counter.setTextColor(getResources().getColor(R.color.solid_yellow));
+                    } else {
+                        counter.setTextColor(getResources().getColor(R.color.solid_white));
+                    }
+
+                    counter.setText(StringUtilities.formatDataNumber(bytesTotal) + " / "
+                            + StringUtilities.formatDataNumber(bytesLimit) + " (" + usage + "%)");
+                } else {
+
+                    counter.setTextColor(getResources().getColor(R.color.solid_white));
+                    counter.setText(StringUtilities.formatDataNumber(bytesTotal));
+                }
+
+            }
+        };
+
+        setListAdapter(adapter);
+
+        // make sure we have up-to-date data
+        if (!Bootstrapper.initializeSystem(this)) {
+
+            /*
+             * if the initialize method was successful, an update has been
+             * issued already
+             */
+            Updater.updateInterfaceStats(this);
+        }
+
+        // clear any notifications that might be up
+        notificationManager.cancel(Configuration.NOTIFICATION_ID_USAGE);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // close cursor
+        mCursor.close();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenuInfo menuInfo) {
+        AdapterView.AdapterContextMenuInfo info;
+        try {
+            info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        } catch (ClassCastException e) {
+            // Log.e(TAG, "Bad menuInfo", e);
+            return;
+        }
+
+        Cursor cursor = (Cursor) getListAdapter().getItem(info.position);
+        if (cursor == null) {
+
+            // For some reason the requested item isn't available, do nothing
+            return;
+        }
+
+        // Setup the menu header
+        menu.setHeaderTitle(cursor.getString(2));
+
+        menu.add(0, InterfaceStatsList.CONTEXT_MENU_ITEM_EDIT, 0, R.string.menu_item_edit);
+        menu.add(0, InterfaceStatsList.CONTEXT_MENU_ITEM_RESET_COUNTERS, 1,
+                R.string.menu_item_reset_counters);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info;
+        try {
+            info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        } catch (ClassCastException e) {
+            // Log.e(TAG, "Bad menuInfo", e);
+            return false;
+        }
+
+        // get the Uri for the interfaceStats item this menu is called upon
+        Uri interfaceStatsUri = ContentUris.withAppendedId(getIntent().getData(), info.id);
+
+        switch (item.getItemId()) {
+
+        case InterfaceStatsList.CONTEXT_MENU_ITEM_RESET_COUNTERS: {
+            Resetter.broadcastResetIntent(getApplicationContext(), interfaceStatsUri);
+            return true;
+        }
+
+        case InterfaceStatsList.CONTEXT_MENU_ITEM_EDIT: {
+
+            /* Display the edit view for the selected record. */
+            startEditorActivity(interfaceStatsUri);
+            return true;
+        }
+        }
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        startEditorActivity(ContentUris.withAppendedId(getIntent().getData(), id));
+    }
+
+    /**
+     * Starts the editor activity for a single interface stats record in the
+     * list.
+     * 
+     * @param interfaceStatsUri
+     *            the item to show the editor activity for.
+     */
+    private void startEditorActivity(Uri interfaceStatsUri) {
+        startActivity(new Intent(Intent.ACTION_EDIT, interfaceStatsUri));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        menu.add(0, OPTION_MENU_ITEM_PREFERENCES, 0, R.string.menu_item_preferences).setIcon(
+                android.R.drawable.ic_menu_preferences);
+
+        menu.add(0, OPTION_MENU_ITEM_ABOUT, 1, R.string.menu_item_about).setIcon(
+                android.R.drawable.ic_menu_help);
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+        case OPTION_MENU_ITEM_PREFERENCES:
+            startActivity(new Intent(ApplicationPreferences.ACTION_EDIT_PREFERENCES));
+            return true;
+
+        case OPTION_MENU_ITEM_ABOUT:
+            showDialog(DIALOG_ABOUT);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        Dialog result = null;
+
+        switch (id) {
+
+        case DIALOG_ABOUT:
+            result = new AboutDialog(InterfaceStatsList.this);
+            break;
+        }
+        return result;
+    }
+}
